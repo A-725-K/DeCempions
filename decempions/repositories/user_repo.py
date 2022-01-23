42d417b1ade70176b521e9d2f73ec937d02351ad
A-725-K <andrea.canepa.12@gmail.com>
2022-01-23 23:22:56 +0100

    Add methods to retrieve users in repository

--- decempions/repositories/user_repo.py
+++ decempions/repositories/user_repo.py
@@ -4,8 +4,10 @@ from werkzeug.security import check_password_hash, generate_password_hash
 class UserRepository():
 	_insert_query = '''
 INSERT INTO User(username, password, email, first_name, last_name)
-VALUES (?,?,?,?,?)
+VALUES (?, ?, ?, ?, ?)
 	'''
+	_find_by_username = 'SELECT username, password FROM User WHERE username = ?'
+	_find_by_email = 'SELECT username, password FROM User WHERE email = ?'
 
 	def create_user(self, username, password, email, first_name, last_name):
 		db = connection.get_db()
@@ -23,5 +25,11 @@ VALUES (?,?,?,?,?)
 		return None
 
 
-	# def find_user_by_username():
-	# def find_user_by_email():
+	def find_user_by_username(username):
+		db = connection.get_db()
+		return db.execute(_find_by_username, (username,))
+
+
+	def find_user_by_email(email):
+		db = connection.get_db()
+		return db.execute(_find_by_email, (email,))
