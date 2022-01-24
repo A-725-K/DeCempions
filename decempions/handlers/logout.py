be177e4ac4f35b12a18e11b4e8a3ddb58d3b52bf
A-725-K <andrea.canepa.12@gmail.com>
2022-01-24 23:40:24 +0100

    Add logout endpoint handler

---
+++ decempions/handlers/logout.py
@@ -0,0 +1,8 @@
+from . import auth_bp
+from decempions.constants import ROUTES
+from flask import redirect, session, url_for
+
+@auth_bp.route(ROUTES['LOGOUT'])
+def logout():
+	session.clear()
+	return redirect(url_for('auth.register'))
