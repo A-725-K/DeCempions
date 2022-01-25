d41493d98c16003d4c0d89a5bde40161b0a7d509
A-725-K <andrea.canepa.12@gmail.com>
2022-01-25 22:25:43 +0100

    Add method in user repository to fetch admin and fix bug in return statement

--- decempions/repositories/user_repo.py
+++ decempions/repositories/user_repo.py
@@ -1,13 +1,16 @@
 from decempions.database import connection
 from werkzeug.security import generate_password_hash
 
-class UserRepository():
+
+class UserRepository:
 	_insert_query = '''
 INSERT INTO User(username, password, email, first_name, last_name)
 VALUES (?, ?, ?, ?, ?)
 	'''
 	_find_by_username = 'SELECT username, password FROM User WHERE username = ?'
+	_find_by_username_all = 'SELECT * FROM User WHERE username = ?'
 	_find_by_email = 'SELECT username, password FROM User WHERE email = ?'
+	_find_admin_by_token = 'SELECT is_admin FROM User WHERE token = ?'
 
 	def create_user(self, username, password, email, first_name, last_name):
 		db = connection.get_db()
@@ -25,11 +28,18 @@ VALUES (?, ?, ?, ?, ?)
 		return None
 
 
-	def find_user_by_username(self, username):
+	def find_user_by_username(self, username, all_fields=False):
 		db = connection.get_db()
-		return db.execute(self._find_by_username, (username,)).fetchone()
+		if not all_fields:
+			return db.execute(self._find_by_username, (username,)).fetchone()
+		return db.execute(self._find_by_username_all, (username,)).fetchone()
 
 
 	def find_user_by_email(self, email):
 		db = connection.get_db()
 		return db.execute(self._find_by_email, (email,)).fetchone()
+
+
+	def find_admin_by_token(self, token):
+		db = connection.get_db()
+		return db.execute(self._find_admin_by_token, (token,)).fetchone()
