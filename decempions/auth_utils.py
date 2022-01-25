d009d79fb57b1b4890fcddbfad26a32c1f4d0a2e
A-725-K <andrea.canepa.12@gmail.com>
2022-01-25 22:21:25 +0100

    Add decorator to check admin requests in api

--- decempions/auth_utils.py
+++ decempions/auth_utils.py
@@ -1,6 +1,7 @@
 import functools
 
-from flask import g, redirect, url_for
+from flask import abort, g, request, redirect, url_for
+from decempions.repositories.user_repo import UserRepository
 
 def login_required(view):
     @functools.wraps(view)
@@ -9,3 +10,18 @@ def login_required(view):
             return redirect(url_for('auth.login'))
         return view(**kwargs)
     return wrapped_view
+
+
+def is_admin_request(view):
+	@functools.wraps(view)
+	def wrapped_view(**kwargs):
+		token = request.headers.get('DC-Token')
+		if token is None:
+			abort(401, description='Not authorized')
+
+		user_repo = UserRepository()
+		if not bool(user_repo.find_admin_by_token(token)):
+			abort(403, description='Forbidden')
+
+		return view(**kwargs)
+	return wrapped_view
