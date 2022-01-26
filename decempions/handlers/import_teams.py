fde2a7dd94b83e6e67880e1f917d4edb53e79164
A-725-K <andrea.canepa.12@gmail.com>
2022-01-26 23:31:57 +0100

    Moved to json support in admin endpoint

--- decempions/handlers/import_teams.py
+++ decempions/handlers/import_teams.py
@@ -2,19 +2,21 @@ from . import admin_bp
 from decempions.auth_utils import is_admin_request
 from decempions.constants import ADMIN_ROUTES, HTTP_METHODS
 from decempions.repositories.team_repo import TeamRepository
-from flask import abort, request
+from flask import abort, jsonify, make_response, request
 
 
 @admin_bp.route(ADMIN_ROUTES['IMPORT_TEAMS'], methods=(HTTP_METHODS['POST'],))
 @is_admin_request
 def import_teams():
 	try:
-		team_names = request.get_data().decode('utf8').split('\n')
+		data = request.get_json()
 		team_repo = TeamRepository()
-		for team in team_names:
-			err = team_repo.create_team(team)
-			if err: raise err
-	except Exception as ex:
-		abort(400, description=f'Error: {ex}')
 
-	return 'OK'
+		for item in data:
+			err = team_repo.create_team(item['team_name'])
+			if err: raise Exception(err)
+
+	except Exception as ex:
+		abort(400, description=f'Error: {str(ex)}')
+
+	return make_response(jsonify('OK'), 200)
