ccfcb0ba6f8036369906dd1e01138ad0be7e505c
A-725-K <andrea.canepa.12@gmail.com>
2022-01-31 00:32:55 +0100

    Add query to update team stats after match

--- decempions/repositories/team_repo.py
+++ decempions/repositories/team_repo.py
@@ -3,13 +3,23 @@ from decempions.database import connection
 class TeamRepository:
 	_insert_query = 'INSERT INTO Team(name) VALUES (?)'
 	_find_id_by_team_name = 'SELECT id FROM Team WHERE name = ?'
-# 	_update_result = '''
-# UPDATE SET
-# 	? = ?+1,
-# 	goal_scored = goal_scored+?,
-# 	goal_conceded=goal_conceded+?
-# WHERE id = ?
-# 	'''
+	_update_match_result = '''
+UPDATE Team
+SET
+	won = won + ?,
+	tie = tie + ?,
+	lost = lost + ?,
+	goal_scored = goal_scored + ?,
+	goal_conceded = goal_conceded + ?,
+	points = points + ?,
+	match_played = match_played + 1
+WHERE id = ?
+	'''
+# 	_get_standing = '''
+# SELECT name, points, won, goal_scored, (goal_scored-goal_conceded) AS goal_diff
+# FROM Team
+# ORDER BY points, won, goal_scored, goal_diff DESC
+# '''
 
 	def create_team(self, team_name):
 		db = connection.get_db()
@@ -27,3 +37,18 @@ class TeamRepository:
 		db = connection.get_db()
 		row = db.execute(self._find_id_by_team_name, (team_name,)).fetchone()
 		return row['id']
+
+
+	def update_match_result(self, id, won, tie, lost, scored, conceded, points):
+		db = connection.get_db()
+		try:
+			db.execute(
+				self._update_match_result,
+				(won, tie, lost, scored, conceded, points, id,)
+			)
+			db.commit()
+		except Exception as e:
+			print(str(e))
+			return 'Error in updating team stats'
+
+		return None
