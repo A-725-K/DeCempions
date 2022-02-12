debd8b2fed729e630b8d79c22ae3a53267b83af4
A-725-K <andrea.canepa.12@gmail.com>
2022-02-12 01:00:09 +0100

    Save user updates into db

--- decempions/handlers/edit_user.py
+++ decempions/handlers/edit_user.py
@@ -1,13 +1,16 @@
 from . import user_bp
 from datetime import datetime
 from decempions.auth_utils import login_required
-from decempions.constants import ROUTES, HTTP_METHODS, TEMPLATES
 from decempions.repositories.user_repo import UserRepository
-from decempions.user_utils import check_name, check_dob
-from flask import (
-	abort, g, flash, jsonify, make_response, render_template, request
+from decempions.repositories.team_repo import TeamRepository
+from decempions.user_utils import check_name, check_dob, check_team
+from decempions.constants import (
+	MONTHS, ROUTES, HTTP_METHODS, SETTINGS, TEMPLATES
 )
-
+from flask import (
+	abort, g, flash, jsonify, make_response, render_template, redirect,
+	request, url_for,
+)
 
 
 @user_bp.route(
@@ -18,23 +21,32 @@ from flask import (
 def edit():
 	user_repo = UserRepository()
 	user = user_repo.find_user_by_username(g.username, all_fields=True)
+	team_repo = TeamRepository()
+	teams = team_repo.find_teams_names_and_ids()
 	curr_year = datetime.now().year
-	print(f'g.username = {g.username}\nuser = {user["username"]}')
 
 	if request.method == HTTP_METHODS['POST']:
-		err = handle_post(user)
+		err = handle_post(user, team_repo, user_repo)
 		if err:
 			flash(err)
+		return redirect(url_for('user.edit'))
 
-	return render_template(TEMPLATES['USER_EDIT'], user=user, year=curr_year)
+	return render_template(
+		TEMPLATES['USER_EDIT'],
+		user=user,
+		year=curr_year,
+		teams=teams,
+		months=MONTHS
+	)
 
 
-def handle_post(user):
+def handle_post(user, team_repo, user_repo):
 	first_name = request.form['first_name']
 	last_name = request.form['last_name']
 	day = request.form['day']
 	month = request.form['month']
 	year = request.form['year']
+	team = request.form['my_team']
 
 	err = check_name(first_name, 'First name')
 	if err: return err
@@ -42,5 +54,22 @@ def handle_post(user):
 	if err: return err
 	err = check_dob(day, month, year)
 	if err: return err
+	err = check_team(team, team_repo)
+	if err: return err
+
+	try:
+		day = int(day)
+		month = int(month)
+		year = int(year)
+		dob = datetime.strptime(f'{year}-{month}-{day}', SETTINGS['DOB_FMT'])
+	except:
+		dob = None
+
+	try:
+		team = int(team)
+	except:
+		team = None
+
+	user_repo.update_user(user['id'], first_name, last_name, dob, team)
 
 	return None
