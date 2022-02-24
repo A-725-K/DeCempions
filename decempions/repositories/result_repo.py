c4c6b756289994ace10931cef13188c0b66928fd
A-725-K <andrea.canepa.12@gmail.com>
2022-02-24 01:18:41 +0100

    Implement repository pattern for Result table

---
+++ decempions/repositories/result_repo.py
@@ -0,0 +1,35 @@
+from decempions.database import connection
+
+class ResultRepository:
+	_insert_query = '''
+INSERT INTO Result(user_id, match_id, guess_goal_home, guess_goal_out)
+VALUES (?, ?, ?, ?)
+	'''
+	_results_by_user_and_matches = lambda _, n: f'''
+SELECT match_id, guess_goal_home, guess_goal_out
+FROM Result
+WHERE user_id = ? AND match_id IN ({("?," * n)[:-1]})
+	'''
+
+
+	def insert_result(self, result):
+		db = connection.get_db()
+		try:
+			db.execute(
+				self._insert_query,
+				(result['user_id'], result['match_id'],
+				result['guess_goal_home'], result['guess_goal_out']),
+			)
+			db.commit()
+		except db.IntegrityError as e:
+			print(str(e))
+			return 'This result already exists'
+
+		return None
+
+	def get_results_by_user_and_matches(self, user_id, matches_ids):
+		db = connection.get_db()
+		return db.execute(
+			self._results_by_user_and_matches(len(matches_ids)),
+			(user_id, *matches_ids,),
+		).fetchall()
