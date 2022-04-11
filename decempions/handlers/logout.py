86daf89d35f25182de97102d2b00143733d639dd
A-725-K <andrea.canepa.12@gmail.com>
2022-04-11 23:37:07 +0200

    Improve layout of user ranking page

--- decempions/handlers/logout.py
+++ decempions/handlers/logout.py
@@ -6,4 +6,4 @@ from flask import redirect, session, url_for
 @auth_bp.route(ROUTES['LOGOUT'])
 def logout():
 	session.clear()
-	return redirect(url_for('auth.register'))
+	return redirect(url_for('auth.login'))
