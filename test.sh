ac4c8eb93efa7364ed55c178c31f1f4f1f32756d
A-725-K <andrea.canepa.12@gmail.com>
2022-08-27 16:12:45 +0200

    Small UI adjustments

--- test.sh
+++ test.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-flask init-db
+# flask init-db
 
-curl -i -X POST 'http://localhost:16000/admin/import-teams' \
---data-binary '@data/squadre.json' -H 'DC-Token: 1234' \
--H 'Content-Type: application/json'
+# curl -i -X POST 'http://localhost:16000/admin/import-teams' \
+# --data-binary '@data/squadre.json' -H 'DC-Token: 1234' \
+# -H 'Content-Type: application/json'
 
 curl -i -X POST 'http://localhost:16000/admin/import-league' \
 --data-binary '@data/campionato.json' -H 'DC-Token: 1234' \
 -H 'Content-Type: application/json'
 
-for i in `seq 1 30`; do
-	echo -e "\n#################################"
-	echo "########## GIORNATA $i ##########"
-	echo -e "#################################\n"
-	curl -i -X POST 'http://localhost:16000/admin/import-week-results' \
-	--data-binary "@data/giornata_$i.json" -H 'DC-Token: 1234' \
-	-H 'Content-Type: application/json'
-done
+# for i in `seq 1 30`; do
+i=2
+echo -e "\n#################################"
+echo "########## GIORNATA $i ##########"
+echo -e "#################################\n"
+curl -i -X POST 'http://localhost:16000/admin/import-week-results' \
+--data-binary "@data/giornata_$i.json" -H 'DC-Token: 1234' \
+-H 'Content-Type: application/json'
+# done
